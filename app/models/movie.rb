fce054522113222ea6adb224fcf4725f5468bd18
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2021-01-12 20:35:24 +0100

    Add validation and validation messages

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,31 @@
 class Movie < ApplicationRecord
+    RATINGS = %w(G PG PG-13 R NC-17)
+
+    validates :title, :released_on, :duration, presence:true
+    validates :description, length: { minimum:25 }  
+    validates :total_gross, numericality: {greater_than_or_equal:0}  
+    validates :image_file_name, format: {
+        with: /\w+\.(jpg|png)\z/i,
+        message: "must be a JPG or PNG image"
+      }
+    validates :rating, inclusion: {in: RATINGS}
+
+    def self.released
+        where("released_on < ?", Time.now).order(released_on: :desc)
+    end
+
+    def self.hits
+        where("total_gross >= 300000000").order(total_gross: :desc)
+    end
+
+    def self.flops
+        where("total_gross < 225000000").order(total_gross: :asc)
+    end
+
+    def self.recently_added
+        order(created_at: :desc).limit(3)
+    end
+
     def flop?
         total_gross < 225000000 || total_gross.blank?
     end
