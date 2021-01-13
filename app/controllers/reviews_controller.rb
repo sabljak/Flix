1626509d64e2fd8e67432a79daa0baf17d3873e2
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2021-01-13 23:06:15 +0100

    Link movies and reviews, add review crud

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,52 @@
+class ReviewsController < ApplicationController
+    
+    before_action :set_movie
+    
+    def index
+        @review = @movie.reviews
+    end
+
+    def new
+        @review = @movie.reviews.new
+    end
+
+    def create
+        @review = @movie.reviews.new(review_params)
+        if @review.save
+            redirect_to movie_reviews_path(@movie),
+            	notice: "Thanks for your review!"
+        else
+           render :new 
+        end
+    end
+
+    def edit
+        @review = @movie.reviews.find(params[:id])
+    end
+
+    def update
+        @review = @movie.reviews.find(params[:id])
+        if @review.update(review_params)
+            redirect_to movie_reviews_path(@movie), notice: "Review successfully updated!" 
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @review = @movie.reviews.find(params[:id])
+        @review.destroy
+        
+        redirect_to movie_reviews_url(@movie), alert: "Review successfully deleted!"
+    end
+
+    private
+
+    def set_movie
+        @movie = Movie.find(params[:movie_id])
+    end
+
+    def review_params
+        params.require(:review).permit(:name, :stars, :comment)
+    end
+end
