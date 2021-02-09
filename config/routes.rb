d93f49b76e6893a661c35984e01f9c99e0a65634
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2021-02-09 02:00:28 +0100

    Add session

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   root "movies#index"
 
   resources :users
+  get "signup" => "users#new"
 
   resources:movies do
     resources:reviews
   end
-  
-  get "signup" => "users#new"
+
+  resource :session, only: [:new, :create, :destroy]
+  get "login" => "sessions#new"
 end
