c488e8cfdace342beccfb5edc902715122d97f19
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2021-02-25 18:42:09 +0100

    Add infinite scroll

--- app/javascript/controllers/movie_controller.js
+++ app/javascript/controllers/movie_controller.js
@@ -2,28 +2,45 @@ import {Controller} from 'stimulus'
 
 export default class extends Controller {
 
-    static targets = ['entries', 'pagination']
+    static targets = ['entries', 'pagination'];
 
-    scroll() {
-        const next_page = this.paginationTarget.querySelector("a[rel='next']");
-        if (next_page) {
-            const url = next_page.href;
-            if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
-                this.loadMovies(url);
+    async initialize() {
+        this.intersectionObserver = new IntersectionObserver(entries => this.processEntries(entries))
+    }
+
+    async processEntries(entries) {
+        for (const entry of entries) {
+            if (entry.isIntersecting) {
+                await this.loadMovies();
+                this.intersectionObserver.unobserve(entry.target);
+                if (entry.target !== this.entriesTarget.lastElementChild)
+                    this.intersectionObserver.observe(this.entriesTarget.lastElementChild)
             }
         }
     }
 
-    async loadMovies(url) {
-        const res = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
-        const json = await res.json();
+    connect() {
+        this.intersectionObserver.observe(this.entriesTarget.lastElementChild)
+    }
 
-        this.entriesTarget.insertAdjacentHTML('beforeend', json.entries);
-        this.paginationTarget.innerHTML = json.pagination;
+    disconnect() {
+        this.intersectionObserver.unobserve(this.entriesTarget.lastElementChild)
+    }
+
+    async loadMovies() {
+        const next_page = this.paginationTarget.querySelector("a[rel='next']");
+        if (next_page) {
+            const res = await fetch(next_page.href, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            const json = await res.json();
+
+            this.entriesTarget.insertAdjacentHTML('beforeend', json.entries);
+            this.paginationTarget.innerHTML = json.pagination;
+        }
+
     }
 }
