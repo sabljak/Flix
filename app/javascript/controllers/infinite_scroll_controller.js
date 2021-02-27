6fe9285c81bf609ad0be5eebc45fc3277c01ccfd
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2021-02-27 12:18:35 +0100

    Add loading spinner

--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -11,7 +11,9 @@ export default class extends Controller {
     async processEntries(entries) {
         for (const entry of entries) {
             if (entry.isIntersecting) {
+                document.querySelector('#loading-spinner').style.display="block";
                 await this.loadMore();
+                document.querySelector('#loading-spinner').style.display="none";
                 this.intersectionObserver.unobserve(entry.target);
                 if (entry.target !== this.entriesTarget.lastElementChild)
                     this.intersectionObserver.observe(this.entriesTarget.lastElementChild)
